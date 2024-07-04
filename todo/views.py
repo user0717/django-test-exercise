b9f00877f5c1d9369408a23caa3f35444717d417
1F10230071 <s1f102300717@iniad.org>
2024-07-04 14:15:19 +0900

    詳細ページを追加

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -18,3 +19,14 @@ def index(request):
        'tasks': tasks 
     }
     return render(request, 'todo/index.html', context)
+
+def detail(request, task_id):
+    try:
+        task = Task.objects.get(pk = task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    
+    context = {
+        'task' : task,
+    }
+    return render(request, 'todo/detail.html', context)
